8bf81a3b6d169ed277db36198c08726d4fd119f5
Yigit Zarbun <yzarbun@gmail.com>
2023-05-30 21:17:39 +0300

    add calendar view

---
+++ src/CalendarSelectedDay.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+function CalendarSelectedDay(props) {
+  const { selectedDay, myEvents } = props;
+  const eventType = "Training";
+  let resultJsx = [];
+  if (selectedDay === null || selectedDay === "") {
+    resultJsx.push(
+      <div className="flex items-center justify-center h-full">
+        <p className="text-center">Select a day to view details</p>
+      </div>
+    );
+  } else if (myEvents.length === 0) {
+    resultJsx.push(
+      <div className="flex items-center justify-center h-full">
+        <p className="text-center">No events available</p>
+      </div>
+    );
+  } else if (selectedDay.length > 0) {
+    resultJsx = (
+      <div>
+        <h2 className="text-center font-bold text-blue-400 text-xl">
+          {selectedDay[0]["event_date"]}
+        </h2>
+
+        <table className="w-full text-left mt-4">
+          <thead>
+            <tr>
+              <th>Event</th>
+              <th>Time</th>
+              <th>Location</th>
+            </tr>
+          </thead>
+          <tbody className="text-sm">
+            {selectedDay.map((s) => (
+              <tr>
+                <td>{eventType}</td>
+                <td>
+                  {s.time < 1000
+                    ? "0" +
+                      s.time.toString()[0] +
+                      ":" +
+                      s.time.toString()[1] +
+                      s.time.toString()[2]
+                    : s.time.toString()[0] +
+                      s.time.toString()[1] +
+                      ":" +
+                      s.time.toString()[2] +
+                      s.time.toString()[3]}
+                </td>
+                <td>
+                  {s.name.length > 9
+                    ? s.name.slice(0, 9).padEnd(11, ".")
+                    : s.name}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="w-1/2 mx-auto mt-6">
+          <button className="w-full text-center border-2 border-blue-500 rounded-md hover:bg-blue-500 p-2 font-bold">
+            Done
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="w-1/4 bg-black text-white rounded-md p-2">{resultJsx}</div>
+  );
+}
+export default CalendarSelectedDay;
--- src/CalendarViewPlayer.js
+++ src/CalendarViewPlayer.js
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getInvites, GET_USER } from "./redux stuff/actions";
+import CalendarSelectedDay from "./CalendarSelectedDay";
 function CalendarViewPlayer() {
+  const dispatch = useDispatch();
+  let { user, invites } = useSelector((store) => store);
+  if (user.player) {
+    user = user.player;
+  }
+  let myEvents = [];
+  if (user && invites && Array.isArray(invites) && invites.length > 0) {
+    myEvents = invites.filter(
+      (i) =>
+        (i.inviter_id === user.player_id || i.invitee_id === user.player_id) &&
+        i.status === "confirmed"
+    );
+  }
   let now = new Date();
   const [selectedYear, setSelectedYear] = useState(now.getFullYear());
   const [selectedMonth, setSelectedMonth] = useState(now.getMonth() + 1);
+  const [selectedDay, setSelectedDay] = useState("");
+  const handleSelectedDay = (value) => {
+    setSelectedDay(value);
+  };
   const years = [];
   for (let y = 2023; y < 2053; y++) {
     years.push(y);
@@ -18,19 +37,28 @@ function CalendarViewPlayer() {
   const handleMonths = (e) => {
     setSelectedMonth(e.target.value);
   };
+  const firstDayOfWeek = new Date(selectedYear, selectedMonth - 1, 1).getDay();
+  const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const adjustedDayNames = [
+    ...dayNames.slice(firstDayOfWeek),
+    ...dayNames.slice(0, firstDayOfWeek),
+  ];
   let daysInMonth = new Date(selectedYear, selectedMonth, 0).getDate();
   let days = [];
   for (let i = 1; i <= daysInMonth; i++) {
     days.push(i);
   }
-
+  useEffect(() => {
+    dispatch({ type: GET_USER });
+    dispatch(getInvites());
+  }, []);
   return (
     <div>
       <div className="bg-heroCalendar bg-bottom bg-cover py-28 rounded-md mt-4"></div>
-      <div className="flex flex-col text-center text-white text-2xl font-bold mt-4">
+      <div className="flex justify-center text-white text-xl font-bold mt-4">
         <select
           onChange={handleYear}
-          className="text-black w-1/5 mx-auto"
+          className="w-1/5 mx-auto bg-inherit text-slate-400 border-2 border-slate-400 rounded-md p-2 hover:bg-blue-500 hover:border-blue-500 hover:text-white"
           defaultValue={selectedYear}
         >
           {years.map((y) => (
@@ -41,25 +69,69 @@ function CalendarViewPlayer() {
         </select>
         <select
           onChange={handleMonths}
-          className="text-black w-1/5 mx-auto"
+          className="w-1/5 mx-auto bg-inherit text-slate-400 border-2 border-slate-400 rounded-md p-2 hover:bg-blue-500 hover:border-blue-500 hover:text-white"
           defaultValue={selectedMonth}
         >
           {months.map((m) => (
             <option key={m} value={m}>
-              {m}
+              {new Date(0, m - 1).toLocaleString("default", { month: "long" })}
             </option>
           ))}
         </select>
       </div>
-      <div className="flex flex-wrap mt-8">
-        {days.map((d) => (
-          <div
-            key={d}
-            className="p-2 calendarWidth border-2 border-blue-300 text-white"
-          >
-            {d}
-          </div>
-        ))}
+      <div className="flex justify-between mt-8">
+        <div className="flex flex-wrap  bg-slate-800 text-white rounded-md p-4 w-3/4 mr-2">
+          {adjustedDayNames.map((day) => (
+            <div
+              key={day}
+              className="p-2 calendarWidth border-2 border-slate-600 bg-slate-950 text-white font-bold text-xl"
+            >
+              {day}
+            </div>
+          ))}
+          {days.map((d) => (
+            <div
+              key={d}
+              className="p-2 calendarWidth border-2 border-slate-600 bg-slate-900 text-white"
+            >
+              <p className="font-bold"> {d}</p>
+              {myEvents.filter(
+                (e) =>
+                  e.event_date ===
+                  `${selectedYear}-${String(selectedMonth).padStart(
+                    2,
+                    0
+                  )}-${String(d).padStart(2, 0)}`
+              ).length > 0 && (
+                <p
+                  onClick={() =>
+                    handleSelectedDay(
+                      myEvents.filter(
+                        (e) =>
+                          e.event_date ===
+                          `${selectedYear}-${String(selectedMonth).padStart(
+                            2,
+                            0
+                          )}-${String(d).padStart(2, 0)}`
+                      )
+                    )
+                  }
+                  className="text-sm text-blue-500 cursor-pointer"
+                >
+                  {myEvents.filter(
+                    (e) =>
+                      e.event_date ===
+                      `${selectedYear}-${String(selectedMonth).padStart(
+                        2,
+                        0
+                      )}-${String(d).padStart(2, 0)}`
+                  ).length + " trainings"}
+                </p>
+              )}
+            </div>
+          ))}
+        </div>
+        <CalendarSelectedDay selectedDay={selectedDay} myEvents={myEvents} />
       </div>
     </div>
   );
